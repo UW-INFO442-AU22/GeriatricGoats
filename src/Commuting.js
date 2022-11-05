72e6366e9206b54eb55b43380d7ec08f724a200a
cassie nguyen <46641077+cbn00@users.noreply.github.com>
2022-11-05 16:10:59 -0700

    Populate more cards (pair-programmed w/ Stella

--- src/Commuting.js
+++ src/Commuting.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import {MakeNavBar } from './Functions.js';
+import React from "react";
+import { MakeNavBar, MakeCard } from "./Functions.js";
 
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 // Commuting Resources go into this page
 
-export function CommutingResources(){
-    return(
-        <div>
-            <MakeNavBar/>
-            <Test yeeHaw="bbbbbb" />
-            
-        </div>
-    )
+export function CommutingResources() {
+  return (
+    <div>
+      <MakeNavBar />
+      <MakeCard
+        imgLink="https://transportation.uw.edu/files/media/nightride-shuttle-inset-image.jpg"
+        title="Night Ride"
+        description="NightRide provides a fare-free, safe and easy way for Husky Card holders to get home at night. NightRide shuttles pick up passengers at eight convenient campus locations and drop off passengers anywhere within one of two zones after the campus location stops."
+      />
+      <MakeCard
+        imgLink="https://i.pinimg.com/originals/00/25/db/0025db4dd4399fb14ed0b5f76bc59e9c.jpg
+        "
+        title="Husky NightWalk"
+        description="Husky NightWalk is safe and easy to use. Uniformed security guards operate 6:30 pm to 2am daily EXCEPT University Holidays, providing a walking escort to community members within the campus locations and the residential area north of 45th St., and to the UW Tower location.  Guards can also assist, with proper identification, building and office lockouts, as well as jump start dead car batteries. Just dial 206.685.WALK (206.685.9255) when you are ready for a safety escort"
+      />
+    </div>
+  );
 }
-
-function Test(props){
-
-    const goToReturn = props.yeeHaw;
-
-    return(
-        <h1>{goToReturn}</h1>
-    )
-
-}
-
--- src/Functions.js
+++ src/Functions.js
@@ -1,58 +1,52 @@
-import React from 'react';
-import { Card, Button, Navbar, Nav, Container, Dropdown, DropdownButton, Table} from 'react-bootstrap';
-import ListGroup from 'react-bootstrap/ListGroup';
-
+import React from "react";
+import {
+  Card,
+  Button,
+  Navbar,
+  Nav,
+  Container,
+  Dropdown,
+  DropdownButton,
+  Table,
+} from "react-bootstrap";
+import ListGroup from "react-bootstrap/ListGroup";
 
 // This is the function page. Various bootstrap functions should go here
-// E.g. our custom Navbars, cards, containers, etc. 
+// E.g. our custom Navbars, cards, containers, etc.
 
 export function MakeNavBar() {
-    return (
-        <Navbar className="mb-4" expand="lg" variant="light" bg="light">
-            <Navbar.Brand href="/" >
-                <img src="./img/Site-logo.png" height="75em" width="180em" alt="sites logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle type="button" aria-controls="navbarNav">
-            </Navbar.Toggle>
-            <Navbar.Collapse id="navbarNav">
-                <Nav className="navbar-nav ml-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="commuting-resources">Commuting Resources</Nav.Link>
-                    <Nav.Link href="other-resources">Other Resources</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-    }
+  return (
+    <Navbar className="mb-4" expand="lg" variant="light" bg="light">
+      <Navbar.Brand href="/">
+        <img
+          src="./img/Site-logo.png"
+          height="75em"
+          width="180em"
+          alt="sites logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle type="button" aria-controls="navbarNav"></Navbar.Toggle>
+      <Navbar.Collapse id="navbarNav">
+        <Nav className="navbar-nav ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="commuting-resources">Commuting Resources</Nav.Link>
+          <Nav.Link href="other-resources">Other Resources</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
-    export function MakeCard(props) {
-        const title= props.Title
-      return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
-      );
-    }
-    
+export function MakeCard(props) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.imgLink} />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>{props.description}</Card.Text>
+        <Button variant="primary">Learn More</Button>
+      </Card.Body>
+    </Card>
+  );
 
-
-
-
-
-
+}
