cbb75e468492a50ae7345e9c7503b89513107dfc
thomasjosephwilson <tommyw2@uw.edu>
2022-11-06 18:59:20 -0800

    rudimentary post

--- src/Home.js
+++ src/Home.js
@@ -19,57 +19,68 @@ export function Home(){
         <div>
             <MakeNavBar/>
             <h1>Home Page</h1>
-            <MakeCard/>
             <MakeButton/>
-
         </div>
     )
 }
 
-// next steps:
-
-// This function will create a button that once clicked, will add a new forum post.
-
-// To code this, need to read through here: https://info340.github.io/dom.html
-// On click, find BackgroundCard()'s body, and append the result of PostCard() to it
-
+  
+// Render button, attach post functionality
 function MakeButton(){
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState([]);
     const handleClick = function(event) {
-        console.log("clicky clicky");
-        setCount(count+1);
+        setCount(<MakePost/>)
+        console.log(count);
     }
 
 return(
-<Button onClick={handleClick} variant="dark">+1</Button> 
+    <span>
+    <MakeCard posts={count}/>
+    <Button onClick={handleClick} variant="dark">+1</Button> 
+    </span>
+
 )
 }
 
-// This function needs to be split into two. 
-// One function should make the first card (the background card), BackgroundCard()
-// and the other function should be make the 'post' card, PostCard()
-function MakeCard(){
+
+// Make post
+// Child component
+function MakePost(){
+    return( 
+        <div>
+    <Card style={{ width: '100%' }}>
+    <Card.Header>Post Title</Card.Header>
+    <Card.Body>
+        <Card.Text> 
+            <Container>
+                <Row>
+                    <Col sm={9}>Ut blanditiis voluptatem dolorem tenetur. Quo dolorum accusamus provident maiores eos.</Col>
+
+                    <Col sm={2} className="d-flex justify-content-end align-items-center">
+                        <Button variant="primary">Upvote</Button>{''}
+                    </Col>
+                </Row>
+            </Container>
+        </Card.Text>
+    </Card.Body>
+</Card>
+        </div>
+);
+}
+
+// Post will be attached here, parent component
+function MakeCard(props){
+
+   const posts = props.posts;
+
+   const postsArray = [];
     return(
+
     <Card style={{ width: '50%' }}>
         <Card.Header as="h5">Night Watch Forum</Card.Header>
         <Card.Body>
-            <Card style={{ width: '100%' }}>
-                <Card.Header>YEEEEEHAWWWW</Card.Header>
-                <Card.Body>
-                    <Card.Text> 
-                        <Container>
-                            <Row>
-                                <Col sm={9}>Ut blanditiis voluptatem dolorem tenetur. Quo dolorum accusamus provident maiores eos.</Col>
-
-                                <Col sm={2} className="d-flex justify-content-end align-items-center">
-                                    <Button variant="primary">Upvote</Button>{' '}
-                                </Col>
-                            </Row>
-                        </Container>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+          {posts}
         </Card.Body>
     </Card>
 
