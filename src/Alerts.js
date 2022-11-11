900daa07578909cb0743decab3265bd899758657
thomasjosephwilson <60676608+thomasjosephwilson@users.noreply.github.com>
2022-11-10 20:34:15 -0800

    alert css

--- src/Alerts.js
+++ src/Alerts.js
@@ -7,9 +7,11 @@ import {
     Navbar,
     Nav,
     Container,
+    Form,
     Dropdown,
     DropdownButton,
     Table,
+    Badge
   } from "react-bootstrap";
 
 // Forum page
@@ -17,12 +19,40 @@ export function Alerts(){
     return(
         <div>
             <MakeNavBar/>
-
             <CardApp/>
+            <MakeForm/>
         </div>
     )
 }
 
+// this will be the form that populates post data
+// need css on container for background
+function MakeForm(){
+    return(
+        <Container >
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </Container>
+    )
+}
 
 // CardApp takes in both MakeCard and MakePost just so the data can be shared
 function CardApp(){
@@ -37,8 +67,9 @@ function CardApp(){
     return (
         
 <div class="container">
-    <h1>Alerts</h1>
-    <Button onClick={clicked}>+</Button>      
+    
+    <Button onClick={clicked} variant="secondary">Create a new post </Button>      
+    <h1 >Alert Hub</h1>
     <MakeCard posts={postList}/>
 
 </div>
@@ -46,40 +77,50 @@ function CardApp(){
 }
   
 
+
+
   // posts are attached here
   // parent component 
 function MakeCard(props){
     const posts = props.posts;
+    // <Card.Header as="h5">Night Watch Forum</Card.Header>
     return(
- 
-     <Card style={{ width: '50%' }}>
-         <Card.Header as="h5">Night Watch Forum</Card.Header>
+ <Card bg="dark" style={{ width: '100%' }}>
          <Card.Body>
             {posts}
          </Card.Body>
      </Card>
+    
  
    );
- 
-   
  }
 
 // Make post
 // Child component
+
+// spacing of columns needs to be fixed (button)
 function MakePost(){
     return( 
         <div>
     <Card style={{ width: '100%' }}>
-    <Card.Header>Post Title</Card.Header>
+    <Card.Header>Posted by u/user • 10 minutes ago</Card.Header>
     <Card.Body>
         <Card.Text> 
             <Container>
                 <Row>
-                    <Col sm={9}>Ut blanditiis voluptatem dolorem tenetur. Quo dolorum accusamus provident maiores eos.</Col>
-
-                    <Col sm={2} className="d-flex justify-content-end align-items-center">
-                        <Button variant="primary">Upvote</Button>{''}
-                    </Col>
+                        <h4> 
+                    Report of attempted buglary on the ave <Badge bg="secondary">crime</Badge>
+                        </h4>
+                </Row>
+                <Row>
+                    <h5>
+                        University Way NE • Seattle
+                    </h5>
+                </Row>
+                <Row>
+                    <h5>
+                    <Badge bg="secondary">upvote image</Badge>  7 upvotes 
+                    </h5>
                 </Row>
             </Container>
         </Card.Text>
