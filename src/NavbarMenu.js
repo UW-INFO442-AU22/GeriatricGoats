a774c35dc2ce305eddf2fa6ca1ec66b67ada8d20
Jennifer Do <jdo17@uw.edu>
2022-11-11 16:53:36 -0800

    Got responsive navbar to work! ＼(￣▽￣)／

--- src/NavbarMenu.js
+++ src/NavbarMenu.js
@@ -1,27 +1,29 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./NavbarMenu.css";
 import logo from "./img/logo.svg";
 
 // Navigation menu at the top of every page
 
 function NavbarMenu() {
-  return (
-    <Navbar className="color-nav" variant="dark">
-      <Navbar.Brand className='navbar-brand' href="/">
-        <img src={logo} alt="Make It Home logo" />
-        <span className="logo-text">make it home</span>
-      </Navbar.Brand>
-      <Navbar.Toggle type="button" aria-controls="navbarNav"></Navbar.Toggle>
-      <Navbar.Collapse id="navbarNav">
-        <Nav className="navbar-nav ml-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="alerts">Alerts</Nav.Link>
-          <Nav.Link href="commute">Commute</Nav.Link>
-          <Nav.Link href="safety">Safety</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+   return (
+      <Navbar collaspseOnSelect expand="lg" className="color-nav" variant="dark">
+         <Container>
+            <Navbar.Brand className='navbar-brand' href="/">
+               <img src={logo} alt="Make It Home logo" />
+               <span className="logo-text">make it home</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="navbarNav">
+               <Nav className="navbar-nav ml-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="alerts">Alerts</Nav.Link>
+                  <Nav.Link href="commute">Commute</Nav.Link>
+                  <Nav.Link href="safety">Safety</Nav.Link>
+               </Nav>
+            </Navbar.Collapse>
+         </Container>
+      </Navbar>
+   );
 }
 
 export default NavbarMenu;
