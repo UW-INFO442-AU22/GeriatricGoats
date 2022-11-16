01d06dc2cbfc9f310630164e52c68c31b33aac7e
Your Name <you@example.com>
2022-11-16 10:23:59 -0800

    login pop up

--- src/App.js
+++ src/App.js
@@ -9,10 +9,17 @@ import { Commute } from "./Commute";
 import { Safety } from "./Safety";
 import { AboutUs } from "./About";
 import { LogIn } from "./LogIn";
+import React from 'react'
+import './App.css'
+import Login from './components/Login'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import Logout from './components/Logout'
+import Welcome from './components/Welcome'
 //import firebase from "firebase/app";
 
 export function App() {
   return (
+    
     <div>
       <NavbarMenu />
       <Routes>
@@ -22,9 +29,21 @@ export function App() {
         <Route path="safety" element={<Safety />} />
         <Route path="about" element={<AboutUs />} />
         <Route path="log-in" element={<LogIn />} />
+
+        <div>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/home" component={Logout} />
+              <Route exact path="/welcome" component={Welcome} />
+            </Switch>
+          </Router>
+        </div>
+
       </Routes>
       <Footer />
     </div>
+    
   );
 }
 
--- src/LogIn.js
+++ src/LogIn.js
@@ -1,6 +1,45 @@
 import "./css/LogIn.css";
+import React from 'react'
+import firebase from '../services/firebase'
+import {useHistory} from 'react-router-dom'
+import './Login.css'
 
-// Log in page
+const Login = () => {
+const history = useHistory();
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then((success) => {
+
+                let user = success.user;
+
+                const obj = {
+                    username : user.displayName,
+                    email:user.email,
+                    uid:user.uid,
+                    img:user.photoURL
+                }
+                console.log(obj)
+                localStorage.setItem('codecaamp',JSON.stringify(obj))
+                history.push('/welcome')
+            })
+            .catch(err => err.message)
+    }
+
+    return (
+        <div className="login-buttons">
+            <div  className="ikmg"><img width="200px" src="https://res.cloudinary.com/codecaamp/image/upload/v1610815107/Purple_Modern_Technology_Gaming_Logo_gqpbv2.png" alt="codecaamp"/></div>
+            <h2 style={{fontFamily:"monospace",fontSize:"40px"}}>Welcome to Codecaamp</h2>
+            <h3>Continue With</h3>
+            <button className="login-provider-button" onClick={signInWithGoogle}>
+                <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon" />
+                <span> Continue with Google</span>
+            </button>
+        </div>
+    )
+}
+
+export default Login
 
 export function LogIn() {
    return (
