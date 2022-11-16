cf2fe1ea0a4e02eda9891fd88c0db21248ac50b6
Your Name <you@example.com>
2022-11-16 10:48:42 -0800

    pop up working

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,45 +1,29 @@
 import "./css/LogIn.css";
-import React from 'react'
-import firebase from '../services/firebase'
-import {useHistory} from 'react-router-dom'
-import './Login.css'
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const Login = () => {
-const history = useHistory();
-    const signInWithGoogle = () => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider)
-            .then((success) => {
 
-                let user = success.user;
+const provider = new GoogleAuthProvider();
 
-                const obj = {
-                    username : user.displayName,
-                    email:user.email,
-                    uid:user.uid,
-                    img:user.photoURL
-                }
-                console.log(obj)
-                localStorage.setItem('codecaamp',JSON.stringify(obj))
-                history.push('/welcome')
-            })
-            .catch(err => err.message)
-    }
-
-    return (
-        <div className="login-buttons">
-            <div  className="ikmg"><img width="200px" src="https://res.cloudinary.com/codecaamp/image/upload/v1610815107/Purple_Modern_Technology_Gaming_Logo_gqpbv2.png" alt="codecaamp"/></div>
-            <h2 style={{fontFamily:"monospace",fontSize:"40px"}}>Welcome to Codecaamp</h2>
-            <h3>Continue With</h3>
-            <button className="login-provider-button" onClick={signInWithGoogle}>
-                <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon" />
-                <span> Continue with Google</span>
-            </button>
-        </div>
-    )
-}
-
-export default Login
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 
 export function LogIn() {
    return (
